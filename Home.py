9916ffe4d8ef5d2919d2bca6de218a346a138337
Sérgio Nascimento <118225965+sergionaskbr@users.noreply.github.com>
2023-10-31 09:59:13 -0300

    Update Home.py

--- Home.py
+++ Home.py
@@ -10,7 +10,7 @@ st.set_page_config( # Junta as páginas.
 image = Image.open('1.png')
 st.sidebar.image(image, width=120)
 
-st.sidebar.markdown('# Cury Company')
+st.sidebar.markdown('# Curry Company')
 st.sidebar.markdown('## Fastest Delivery in Town')
 st.sidebar.markdown("""---""")
 
@@ -18,7 +18,7 @@ st.write('# Curry Company Growth Dashboard')
 
 st.markdown(
     """
-    Growth Dashboard foi construído para acompanhar as métricas de crescimento dos Empregadores e Restaurantes.
+    Growth Dashboard foi construído para acompanhar as métricas de crescimento da empresa Curry Company.
     ### Como utilizar esse Growth Dashboard?
     - Visão Empresa: 
         - Visão Gerencial: Métricas gerais de comportamento.
@@ -31,5 +31,6 @@ st.markdown(
     ### Ask for Help
     - Time de Data Science do Discord: 
         - @sergionask
+    Dados extraídos de Food Delivery Dataset (Kaggle) - https://www.kaggle.com/datasets/gauravmalik26/food-delivery-dataset/
         
 """)
